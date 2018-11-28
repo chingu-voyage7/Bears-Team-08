8a9ca532b9ec0cacff73fd52229c18ca71b73a4a
Luc Somers <lucwsomers@gmail.com>
2018-11-28 13:26:59 -0200

    UPDATE - swagger

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { UserProvider } from '../src/context/User';
+import { UserProvider } from './context/User';
 import Header from './components/header/Header';
 import Main from './components/Main';
 
@@ -8,12 +8,11 @@ const App = () => (
   <UserProvider>
     <Router>
       <div className="App">
-          <Header />
-          <Main />
+        <Header />
+        <Main />
       </div>
     </Router>
   </UserProvider>
-  
 );
 
 export default App;
